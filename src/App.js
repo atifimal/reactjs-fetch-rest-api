3e72fa2bde0c6a7d24b8098e77fd9f223bcfd343
Atif Imal <mehmetatif@mail.ru>
2022-06-18 17:15:52 +0300

    changed some comment lines

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import './App.css'
 
-//Consider render() method like main() method, so this class will be executed with export default
+//we export this class and it'll be imported in index.js with the App component.
 export default class FetchData extends React.Component {
   state = { //the object that we use it store data and check if it's done with loading
     loading: true,
@@ -19,6 +19,7 @@ export default class FetchData extends React.Component {
     })
   }
 
+  //index.js calls this method. So you can consider that it's working like main() method
   render() {
     if (this.state.loading) {
       return (<div>Loading</div>);
@@ -31,7 +32,7 @@ export default class FetchData extends React.Component {
     return (
       <div className="App">
         <ul className="meme-list">
-          {this.state.jsondata.map(meme => ( //map() works like array loop in here
+          {this.state.jsondata.map(meme => ( //map() works like array loop
             <li>
               <h1 className="meme-head">{meme.name}</h1>
               <img className="meme-img" src={meme.url} />
